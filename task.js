be7c84268436ab4cfe06e3e635ee130bebafd596
Matthew <mngz636@gmail.com>
2026-09-22 03:09:05 +0200

    Create task.js

---
+++ task.js
@@ -0,0 +1,53 @@
+//   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+jQuery.getJSON('http://websitescraper.herokuapp.com/?url=http://ichart.finance.yahoo.com/table.csv?s=RIL.BO&callback=?', function (csvdata) {
+  console.log(csvdata.csvToArray());
+});
+  
+function ExportToTable() {  
+            var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv)$/;  
+      
+            //Checks whether the file is a valid csv file    
+            if (regex.test($("#csvfile").val().toLowerCase())) {  
+                //Checks whether the browser supports HTML5    
+                if (typeof(FileReader) != "undefined") {  
+                    var reader = new FileReader();  
+                    
+                    reader.onload = function(e) {  
+                        var table = $("#csvtable > tbody");  
+         
+                        //Splitting of Rows in the csv file    
+                        var csvrows = e.target.result.split("\n");  
+                        
+                        for (var i = 0; i < csvrows.length; i++) {  
+                            if (csvrows[i] != "") {  
+                                var row = "<tr>";  
+                                var csvcols = csvrows[i].split(",");  
+                           
+                                //Looping through each cell in a csv row    
+                                for (var j = 0; j < csvcols.length; j++) {  
+                                    var cols = "<td>" + csvcols[j] + "</td>";  
+                                    
+                                    row += cols;  
+                                }  
+           
+                                row += "</tr>";  
+           
+                                table.append(row);  
+                            }  
+                        }  
+                        
+                        $('#csvtable').show();  
+                    }  
+                    
+                    reader.readAsText($("#csvfile")[0].files[0]);  
+                } else {  
+                    alert("Sorry! Your browser does not support HTML5!");  
+                }  
+            } else {  
+                alert("Please upload a valid CSV file!");  
+            }  
+        }
+ 
+   // <input type="file" id="csvfile" />  
+  //<input class="form-control btn" type="button" id="viewfile" value="Turn Key" onclick="ExportToTable();" />  
+  
